104595759da2d090f69a655f66b5e261519cb004
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-02-15 12:55:12 +0900

    Update pyupbit_example_03.py

--- pyupbit_example/pyupbit_example_03.py
+++ pyupbit_example/pyupbit_example_03.py
@@ -3,8 +3,8 @@ import pyupbit
 orderbook = pyupbit.get_orderbook("KRW-ETH")
 print(orderbook)
 
-access = "yKbt3K7Zi3xVO07DypiyZDQLLXygzcy3tlVs5njC"
-secret = "LGN1OKUAjbYy3eRgzXE4cD4EhuJZBSPxO5fge0aL"
+access = "##############################"
+secret = "##############################"
 
 upbit = pyupbit.Upbit(access,secret)
 print(upbit.get_balances())
