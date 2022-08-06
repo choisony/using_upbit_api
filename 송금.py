ce9e77b109cf9f7d79d1d9d6344d715163bce085
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-08-06 23:23:01 +0900

    O

--- 송금.py
+++ 송금.py
@@ -1,48 +1,22 @@
-# import jwt
-# import hashlib
-# import os
-# import requests
-# import uuid
-# from urllib.parse import urlencode, unquote
-# ACCESS_KEY = "lkh1LjYfDtjrkERW1GntUbATbTKTYZQnEgp9IHUb"
-# SECRET_KEY = 'gUlxoEwFwZHokIgXmsZXZZcjrlZIBQY0kwMEVwhQ'
-# SERVER_URL = 'https://api.upbit.com'
-#
-# payload = {
-#     'access_key': ACCESS_KEY,
-#     'nonce': str(uuid.uuid4()),
-# }
-#
-# jwt_token = jwt.encode(payload, SECRET_KEY)
-# authorization = 'Bearer {}'.format(jwt_token)
-# headers = {
-#   'Authorization': authorization,
-# }
-#
-# res = requests.get(SERVER_URL + '/v1/accounts', headers=headers)
-# currency_data = res.json()
-#
-# print("보유 자본")
-# print(f"{currency_data[0]['currency']}: {currency_data[0]['balance']}")
-# # for i in currency_data:
-# #     print(f"{i['currency'],i['balance']}")
-
 import jwt
 import hashlib
-import os
 import requests
 import uuid
 from urllib.parse import urlencode, unquote
 
-ACCESS_KEY = ""
-SECRET_KEY = ''
+ACCESS_KEY = "" #ACCESS_KEY 를 입력해주세요.
+SECRET_KEY = '' #SECRET_KEY 를 입력해주세요.
 SERVER_URL = 'https://api.upbit.com'
 
-params = {
-  'currency': 'TRX',
-  'amount': '15',
-  'address': 'TP9ouEWav4j5KnzScpvL8Lt2CPNBvqABGn'  #내 트론 주소
-}
+what = input("어떤 코인을 송금 하실건가요? ex.('TRX','SOL'): ")
+amount = input("몇개 송금 하실건가요? :")
+address = input("어느 주소로 송금 하실건가요? :")
+params = {}
+params['currency'] = what
+params['amount'] = amount
+params['address'] = address
+
+print(params)
 query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
 
 m = hashlib.sha512()
@@ -66,3 +40,15 @@ res = requests.post(SERVER_URL + '/v1/withdraws/coin',json = params, headers=hea
 tt = res.json()
 
 print(tt)
+
+
+
+RESULT : 
+  
+어떤 코인을 송금 하실건가요? ex.('TRX','SOL'): WEMIX
+몇개 송금 하실건가요? :2.72777777
+어느 주소로 송금 하실건가요? :0x7b636C238fAe8EAD177e3A8601E47503C66a615d
+  
+{'currency': 'WEMIX', 'amount': '2.72777777', 'address': '0x7b636C238fAe8EAD177e3A8601E47503C66a615d'}
+{'type': 'withdraw', 'uuid': '67d18d87-6e08-4e4b-8443-a1cb930760ee', 'currency': 'WEMIX', 'txid': None, 'state': 'WAITING', 
+    'created_at': '2022-08-06T23:20:50+09:00', 'done_at': None, 'amount': '2.72777777', 'fee': '0.05', 'transaction_type': 'default'}
